086663990c7c767fe2a896b2469e20c0ee59c965
Swapnil Kale <swapnil2304@users.noreply.github.com>
2023-04-22 10:34:54 +0530

    Create openchat.py

---
+++ openchat.py
@@ -0,0 +1,16 @@
+import openai
+openai.api_key = "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"
+while True:
+  model_engine="text-davinci-003"
+  prompt = input("Enter prompt:")
+  if 'exit' in prompt or 'quit' in prompt:
+    break
+  completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1, stop=None,
+    temperature=0.5)
+  response = completion.choices[0].text
+  print(response)  
+    
